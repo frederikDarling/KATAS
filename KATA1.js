a14094f69120e52fe3c36853fa557a4c36046dcd
frederikDarling <56248424+frederikDarling@users.noreply.github.com>
2023-01-12 09:56:25 +0100

    Kata 1 now with comments

--- KATA1.js
+++ KATA1.js
@@ -1,17 +1,27 @@
+//Declare function.
 function reorder(aString){
 
+   //Declare and intialize an empty string.
    let newString = "";
 
+   //Find all of the uppercase letters by replacing all other characters by "".
    let upperCase = aString.replace(/[^A-Z]/g, '');
+   // Add uppercase letters to newString.
    newString += upperCase;
 
+   //Find all of the lowercase letters by replacing all other characters by "".
    let lowerCase = aString.replace(/[^a-z]/g, ''); 
+   // Add lowercase letter to newString.
    newString += lowerCase;
 
+   //Find all of the numbers by replacing all other characters by "".
    let numbers = aString.replace(/[^0-9]/g, '');
+   //Add numbers to newString.
    newString += numbers;
    
+   //Return the newString
    return newString;
 }
 
+//Console log the reorder() function to print the reordered string.
 console.log(reorder("hA2p4Py"));
